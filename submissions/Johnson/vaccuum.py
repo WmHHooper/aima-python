ed516425de6eb87490c8d8ebb818415d50311251
JohnsonLk <luke.johnson@pop.belmont.edu>
2016-08-27 13:19:09 -0500

    Update vaccuum.py
    So it references the pictures when ".../submissions/Johnson" is selected as the working directory.

--- submissions/Johnson/vaccuum.py
+++ submissions/Johnson/vaccuum.py
@@ -198,10 +198,10 @@ v.scatter_things(Dirt)
 g = gui.EnvGUI(v, 'Vaccuum')
 c = g.getCanvas()
 c.mapImageNames({
-    ag.Wall: 'images/Johnson/default.jpg',
+    ag.Wall: 'default.jpg',
     # Floor: 'images/floor.png',
-    Dirt: 'images/dirt.png',
-    ag.Agent: 'images/vacuum.png',
+    Dirt: '../../images/dirt.png',
+    ag.Agent: '../../images/vacuum.png',
 })
 c.update()
 g.mainloop()
