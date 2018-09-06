9b4480b1b6e7e067fc765d22fad239151932e3ad
Enum81 <enumscratch81@gmail.com>
2018-09-05 23:31:01 -0500

    Updated settings

--- submissions/aardvark/vacuum2.py
+++ submissions/aardvark/vacuum2.py
@@ -8,10 +8,60 @@ def HW2Agent() -> object:
             action = 'Suck'
         else:
             lastBump, lastStatus,  = program.oldPercepts[-1]
-            if bump == 'None':
-                action = 'Right'
-            else:
-                action = 'Left'
+            lastAction = program.oldActions[-1]
+            if program.counter == 0:
+                if bump == 'Bump':
+                    program.counter += 1
+                    action = 'Right'
+                else:
+                    action = 'Down'
+            elif program.counter == 1:
+                if bump == 'Bump':
+                    program.counter = 6
+                    action = 'Up'
+                else:
+                    program.counter += 1
+                    action = 'Up'
+            elif program.counter == 2:
+                if bump == 'Up':
+                    program.counter += 1
+                    action = 'Up'
+                else:
+                    action = 'Up'
+            elif program.counter == 3:
+                if bump == 'Bump':
+                    program.counter = 7
+                    action = 'Left'
+                else:
+                    program.counter = 0
+                    action = 'Down'
+            #Skipping 4 and 5 because it's similar to 1 and 3
+            elif program.counter == 6:
+                if bump == 'Bump':
+                    program.counter += 1
+                    action = 'Left'
+                else:
+                    action = 'Up'
+            elif program.counter == 7:
+                if bump == 'Bump':
+                    program.counter = 3
+                    action = 'Right'
+                else:
+                    program.counter += 1
+                    action = 'Down'
+            elif program.counter == 8:
+                if bump == 'Bump':
+                    program.counter += 1
+                    action = 'Left'
+                else:
+                    action = 'Down'
+            elif program.counter == 9:
+                if bump == 'Bump':
+                    program.counter = 1
+                    action = 'Right'
+                else:
+                    program.counter = 6
+                    action = 'Up'
 
         program.oldPercepts.append(percept)
         program.oldActions.append(action)
@@ -19,7 +69,9 @@ def HW2Agent() -> object:
 
     # assign static variables here
     program.oldPercepts = [('None', 'Clean')]
-    program.oldActions = ['NoOp']
+    program.oldActions = ['Left', 'Right']
+    program.counter = 0
+    # program.lastWall = ['None', 'Down']
 
     agt = ag.Agent(program)
     # assign class attributes here:
